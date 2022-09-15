fe50c92ded2c899c3d95619f0c35c812b202e58c
Hector Perez <hector.elliott.perez@gmail.com>
2022-09-15 14:22:48 -0700

    finished isFive test

--- code.js
+++ code.js
@@ -3,20 +3,28 @@ function helloWorld() {
     return "Hello, World!";
 }
 
+// function sayHello(name) {
+//     if (name === undefined) {
+//         return "Hello, World!";
+//     } else if (typeof(name) === "boolean") {
+//         return "Hello, World!";
+//     }
+//     return ("Hello, " + name + "!")
+// }
+
 function sayHello(name) {
-    if (name === undefined) {
-        return "Hello, World!";
-    } else if (typeof(name) === "boolean") {
+    if(typeof(name) !== "string") {
         return "Hello, World!";
     }
-    return ("Hello, " + name + "!")
+    return "Hello, " + name + "!";
 }
 
-console.log(sayHello())
-
-//     if (name === "") {
-//         return "Hello, World!";
-//     } else {
-//         return ("Hello, " + name + "!");
-//     }
-// }
+function isFive(input) {
+    if (typeof(input) == 5) {
+        return true;
+    } else if (input == "5") {
+        return true;
+    }
+    return false;
+}
+console.log(isFive(5));
